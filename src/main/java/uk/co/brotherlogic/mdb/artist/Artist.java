3eeb163277daec88cc43c72ec6fb6a98cee6fae0
sat <sat@2cd3a4d7-486e-4978-93eb-ba037767b44d>
2009-11-29 12:37:46 +0000

    Added the old files
    git-svn-id: svn+ssh://hancock/var/svn/mdb/mdbcore/trunk@4 2cd3a4d7-486e-4978-93eb-ba037767b44d

---
+++ src/main/java/uk/co/brotherlogic/mdb/artist/Artist.java
@@ -0,0 +1,147 @@
+package uk.co.brotherlogic.mdb.artist;
+
+import java.sql.SQLException;
+import java.util.Collection;
+
+import uk.co.brotherlogic.mdb.Utils;
+import uk.co.brotherlogic.mdb.record.GetRecords;
+import uk.co.brotherlogic.mdb.record.Record;
+
+/**
+ * Application level artist object
+ * 
+ * @author Simon Tucker
+ */
+public class Artist implements Comparable<Artist>
+{
+	/** The id number of the artist */
+	private int id;
+
+	/** The sort name */
+	private String sortName;
+
+	/** The show name */
+	private String showName;
+
+	/**
+	 * Empty constructor
+	 */
+	public Artist()
+	{
+		id = -1;
+	}
+
+	/**
+	 * Constructor
+	 * 
+	 * @param sort
+	 *            The sort name
+	 * @param show
+	 *            The show name
+	 * @param idNumber
+	 *            The id number
+	 */
+	public Artist(final String sort, final String show, final int idNumber)
+	{
+		this.sortName = sort;
+		this.id = idNumber;
+		this.showName = show;
+	}
+
+	@Override
+	public final int compareTo(final Artist o)
+	{
+		return -sortName.toLowerCase().compareTo(o.sortName.toLowerCase());
+	}
+
+	/**
+	 * String method for representing the object in a list
+	 * 
+	 * @return A {@link String} of how the object should be represented in a
+	 *         list
+	 */
+	public final String displayInList()
+	{
+		return sortName;
+	}
+
+	@Override
+	public final boolean equals(final Object o)
+	{
+		if (o instanceof Artist)
+			return this.compareTo((Artist) o) == 0;
+		else
+			return false;
+	}
+
+	/**
+	 * Get the id number for this artist
+	 * 
+	 * @return The ID number
+	 */
+	public final int getId()
+	{
+		return id;
+	}
+
+	public Collection<Record> getRecords() throws SQLException
+	{
+		return GetRecords.create().getRecordsWithPers(id);
+	}
+
+	/**
+	 * Get the show name
+	 * 
+	 * @return {@link String} of the Show name
+	 */
+	public final String getShowName()
+	{
+		return showName;
+	}
+
+	/**
+	 * Get the sorting name
+	 * 
+	 * @return {@link String} of the name used for sorting
+	 */
+	public final String getSortName()
+	{
+		return sortName;
+	}
+
+	@Override
+	public final int hashCode()
+	{
+		return sortName.hashCode();
+	}
+
+	/**
+	 * Saves this artist
+	 * 
+	 * @throws SQLException
+	 *             if the DB connection fails
+	 */
+	public final void save() throws SQLException
+	{
+		if (id == -1)
+			id = GetArtists.create().saveArtist(this);
+	}
+
+	@Override
+	public final String toString()
+	{
+		return sortName;
+	}
+
+	/**
+	 * Static builder method given a comma'd name
+	 * 
+	 * @param name
+	 *            the Name with a comma (e.g. "Turner, Ike")
+	 * @return A valid artist for the given name
+	 */
+	public static Artist build(final String name)
+	{
+		return new Artist(name, Utils.flipString(name), -1);
+	}
+}
---
+++ src/main/java/uk/co/brotherlogic/mdb/record/RecordScore.java
@@ -0,0 +1,79 @@
+package uk.co.brotherlogic.mdb.record;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import uk.co.brotherlogic.mdb.Connect;
+
+public class RecordScore
+{
+	Record rec;
+
+	boolean[] changed = new boolean[1];
+	private static int SIMON_RANK_CHANGED = 0;
+	private double simonRank;
+
+	public void save() throws SQLException
+	{
+		System.out.println("Saving: " + rec.getNumber());
+		if (rec != null)
+		{
+			String sql = "UPDATE score_table ";
+
+			for (int i = 0; i < changed.length; i++)
+				if (changed[i])
+					if (i == 0)
+						sql += "SET simon_rank = ?";
+
+			sql += " WHERE record_id = ?";
+
+			PreparedStatement ps = Connect.getConnection()
+					.getPreparedStatement(sql);
+
+			int sCount = 1;
+			for (int i = 0; i < changed.length; i++)
+				if (changed[i])
+				{
+					if (i == 0)
+						ps.setDouble(sCount, simonRank);
+					sCount++;
+				}
+
+			ps.setInt(sCount, rec.getNumber());
+			System.out.println(ps);
+
+			ps.execute();
+			ps.close();
+
+			// Update the record to ranked
+			rec.setState(Record.RANKED);
+			rec.save();
+		}
+	}
+
+	public void setSimonRank(double rank)
+	{
+		System.out.println("Setting rank: " + rank);
+		simonRank = rank;
+		changed[SIMON_RANK_CHANGED] = true;
+	}
+
+	public static RecordScore get(Record rec) throws SQLException
+	{
+		String sql = "SELECT * from score_table WHERE record_id = ?";
+		PreparedStatement ps = Connect.getConnection()
+				.getPreparedStatement(sql);
+		ps.setInt(1, rec.getNumber());
+		ResultSet rs = ps.executeQuery();
+		if (rs.next())
+		{
+			RecordScore score = new RecordScore();
+			score.simonRank = rs.getDouble("simon_rank");
+			score.rec = rec;
+			return score;
+		}
+		else
+			throw new SQLException("Record " + rec + " not found!");
+	}
+}
