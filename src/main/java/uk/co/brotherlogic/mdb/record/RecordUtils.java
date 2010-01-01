c7c1506d663881b22eaf281c2951d5a4e9fed32a
Simon Tucker <s.tucker@dcs.shef.ac.uk>
2010-01-01 20:55:23 +0000

    Added Utils class to deal with misc record actions

---
+++ src/main/java/uk/co/brotherlogic/mdb/record/RecordUtils.java
@@ -0,0 +1,81 @@
+package uk.co.brotherlogic.mdb.record;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import uk.co.brotherlogic.mdb.Connect;
+
+public class RecordUtils
+{
+	private static Record getNewRecord(String baseformat)
+	{
+		try
+		{
+			String sql = "SELECT recordnumber from formats,records LEFT JOIN score_table ON recordnumber = record_id WHERE format = formatnumber AND baseformat = ? AND simon_score IS NULL";
+			PreparedStatement ps = Connect.getConnection().getPreparedStatement(sql);
+			ps.setString(1, baseformat);
+			ResultSet rs = ps.executeQuery();
+			if (rs.next())
+				return GetRecords.create().getRecord(rs.getInt(1));
+		}
+		catch (SQLException e)
+		{
+			e.printStackTrace();
+		}
+
+		return null;
+	}
+
+	private static Record getRecord(String baseformat, int listenCount, int months)
+	{
+		try
+		{
+			String sql = "SELECT recordnumber from formats,records LEFT JOIN score_table ON recordnumber = record_id WHERE format = formatnumber AND baseformat = ? AND simon_rank_count = ? AND boughtdate < 'today'::date - "
+					+ months
+					+ "*'1 month'::interval AND simon_score > 5  ORDER BY boughtdate DESC LIMIT 1";
+			PreparedStatement ps = Connect.getConnection().getPreparedStatement(sql);
+			ps.setString(1, baseformat);
+			ps.setInt(2, listenCount);
+			ResultSet rs = ps.executeQuery();
+			if (rs.next())
+				return GetRecords.create().getRecord(rs.getInt(1));
+		}
+		catch (SQLException e)
+		{
+			e.printStackTrace();
+		}
+
+		return null;
+	}
+
+	public static Record getRecordToListenTo(String baseformat)
+	{
+		Record r = getNewRecord(baseformat);
+		if (r == null)
+			r = getRecord(baseformat, 2, 6);
+		if (r == null)
+			r = getRecord(baseformat, 1, 3);
+		return r;
+	}
+
+	public static Record getRecordToListenTo(String[] baseformats)
+	{
+		Record toRet = null;
+
+		for (String string : baseformats)
+		{
+			Record r = getRecordToListenTo(string);
+			if (toRet == null || r.getDate().after(toRet.getDate()))
+				toRet = r;
+		}
+
+		return toRet;
+	}
+
+	public static void main(String[] args)
+	{
+		Connect.setForProduction();
+		System.out.println(RecordUtils.getRecordToListenTo(new String[] { "7", "12", "10" }));
+	}
+}
