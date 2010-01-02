861a43f44ed8e67c1e7f0a70ba346776812d625f
Simon Tucker <s.tucker@dcs.shef.ac.uk>
2010-01-02 09:23:38 +0000

    Added the get ripped method to the record utils

--- src/main/java/uk/co/brotherlogic/mdb/record/RecordUtils.java
+++ src/main/java/uk/co/brotherlogic/mdb/record/RecordUtils.java
@@ -35,18 +35,18 @@ public class RecordUtils
 	public static void main(String[] args) throws SQLException
 	{
 		Connect.setForProduction();
-		System.out.println(RecordUtils.getRecordToListenTo(new String[] { "7", "12", "10" }));
+		System.out.println(RecordUtils.getRecordToListenTo(new String[] { "CD" }));
 	}
 
 	private static Record getNewRecord(String baseformat) throws SQLException
 	{
-		String cd_extra = "AND ripped = 'true'";
+		String cd_extra = "AND riploc IS NOT NULL";
 
 		if (!baseformat.equalsIgnoreCase("cd"))
 			cd_extra = "";
 
 		String sql = "SELECT recordnumber from formats,records LEFT JOIN score_table ON recordnumber = record_id WHERE format = formatnumber "
-				+ cd_extra + "AND baseformat = ? AND simon_score IS NULL";
+				+ cd_extra + " AND baseformat = ? AND simon_score IS NULL";
 		PreparedStatement ps = Connect.getConnection().getPreparedStatement(sql);
 		ps.setString(1, baseformat);
 		ResultSet rs = ps.executeQuery();
@@ -57,13 +57,13 @@ public class RecordUtils
 
 	private static Record getRecord(String baseformat, int listenCount, int months) throws SQLException
 	{
-		String cd_extra = "AND ripped = 'true' AND";
+		String cd_extra = "AND riploc IS NOT NULL";
 
 		if (!baseformat.equalsIgnoreCase("cd"))
 			cd_extra = "";
 
 		String sql = "SELECT recordnumber from formats,records LEFT JOIN score_table ON recordnumber = record_id WHERE format = formatnumber "
-				+ cd_extra + "AND baseformat = ? AND simon_rank_count = ? AND boughtdate < 'today'::date - " + months
+				+ cd_extra + " AND baseformat = ? AND simon_rank_count = ? AND boughtdate < 'today'::date - " + months
 				+ "*'1 month'::interval AND simon_score > 5  ORDER BY boughtdate DESC LIMIT 1";
 		PreparedStatement ps = Connect.getConnection().getPreparedStatement(sql);
 		ps.setString(1, baseformat);
@@ -74,4 +74,15 @@ public class RecordUtils
 
 		return null;
 	}
+
+	private static Record getRecordToRip() throws SQLException
+	{
+		String sql = "SELECT recordnumber from records,formats WHERE baseformat = 'CD' AND riploc IS NULL";
+		PreparedStatement ps = Connect.getConnection().getPreparedStatement(sql);
+		ResultSet rs = ps.executeQuery();
+		if (rs.next())
+			return GetRecords.create().getRecord(rs.getInt(1));
+		return null;
+	}
+
 }
