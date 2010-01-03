831f901030fd5db36aaa1e8e2276e1c7c5ca55d1
Simon Tucker <s.tucker@dcs.shef.ac.uk>
2010-01-03 09:34:51 +0000

    Ordered the to rip records by owner

--- src/main/java/uk/co/brotherlogic/mdb/record/RecordUtils.java
+++ src/main/java/uk/co/brotherlogic/mdb/record/RecordUtils.java
@@ -9,49 +9,9 @@ import uk.co.brotherlogic.mdb.Connect;
 
 public class RecordUtils
 {
-	private static Record getNewRecord(String baseformat) throws SQLException
-	{
-		String cd_extra = "AND riploc IS NOT NULL";
-
-		if (!baseformat.equalsIgnoreCase("cd"))
-			cd_extra = "";
-
-		String sql = "SELECT recordnumber from formats,records LEFT JOIN score_table ON recordnumber = record_id WHERE format = formatnumber "
-				+ cd_extra + " AND baseformat = ? AND simon_score IS NULL";
-		PreparedStatement ps = Connect.getConnection().getPreparedStatement(sql);
-		ps.setString(1, baseformat);
-		ResultSet rs = ps.executeQuery();
-		if (rs.next())
-			return GetRecords.create().getRecord(rs.getInt(1));
-		return null;
-	}
-
-	private static Record getRecord(String baseformat, int listenCount, int months)
-			throws SQLException
-	{
-		String cd_extra = "AND riploc IS NOT NULL";
-
-		if (!baseformat.equalsIgnoreCase("cd"))
-			cd_extra = "";
-
-		String sql = "SELECT recordnumber from formats,records LEFT JOIN score_table ON recordnumber = record_id WHERE format = formatnumber "
-				+ cd_extra
-				+ " AND baseformat = ? AND simon_rank_count = ? AND boughtdate < 'today'::date - "
-				+ months
-				+ "*'1 month'::interval AND simon_score > 5  ORDER BY boughtdate DESC LIMIT 1";
-		PreparedStatement ps = Connect.getConnection().getPreparedStatement(sql);
-		ps.setString(1, baseformat);
-		ps.setInt(2, listenCount);
-		ResultSet rs = ps.executeQuery();
-		if (rs.next())
-			return GetRecords.create().getRecord(rs.getInt(1));
-
-		return null;
-	}
-
 	public static Record getRecordToListenTo(String baseformat) throws SQLException
 	{
-		//Only listen to new records on a Sunday!
+		// Only listen to new records on a Sunday!
 		Calendar today = Calendar.getInstance();
 		Record r = null;
 		if (today.get(Calendar.DAY_OF_WEEK) == Calendar.SUNDAY)
@@ -79,11 +39,11 @@ public class RecordUtils
 
 	public static Record getRecordToRip() throws SQLException
 	{
-		String sql = "SELECT recordnumber from records,formats WHERE baseformat = 'CD' AND format = formatnumber AND riploc IS NULL";
+		String sql = "SELECT recordnumber from records,formats WHERE baseformat = 'CD' AND format = formatnumber AND riploc IS NULL ORDER BY owner";
 		PreparedStatement ps = Connect.getConnection().getPreparedStatement(sql);
 		ResultSet rs = ps.executeQuery();
-		if (rs.next())
-			return GetRecords.create().getRecord(rs.getInt(1));
+		while (rs.next())
+			return (GetRecords.create().getRecord(rs.getInt(1)));
 		return null;
 	}
 
@@ -93,6 +53,44 @@ public class RecordUtils
 		System.out.println(RecordUtils.getRecordToListenTo(new String[] { "7" }));
 		System.out.println(RecordUtils.getRecordToListenTo(new String[] { "12" }));
 		System.out.println(RecordUtils.getRecordToListenTo(new String[] { "10" }));
+		System.out.println(getRecordToRip());
+	}
+
+	private static Record getNewRecord(String baseformat) throws SQLException
+	{
+		String cd_extra = "AND riploc IS NOT NULL";
+
+		if (!baseformat.equalsIgnoreCase("cd"))
+			cd_extra = "";
+
+		String sql = "SELECT recordnumber from formats,records LEFT JOIN score_table ON recordnumber = record_id WHERE format = formatnumber "
+				+ cd_extra + " AND baseformat = ? AND simon_score IS NULL";
+		PreparedStatement ps = Connect.getConnection().getPreparedStatement(sql);
+		ps.setString(1, baseformat);
+		ResultSet rs = ps.executeQuery();
+		if (rs.next())
+			return GetRecords.create().getRecord(rs.getInt(1));
+		return null;
+	}
+
+	private static Record getRecord(String baseformat, int listenCount, int months) throws SQLException
+	{
+		String cd_extra = "AND riploc IS NOT NULL";
+
+		if (!baseformat.equalsIgnoreCase("cd"))
+			cd_extra = "";
+
+		String sql = "SELECT recordnumber from formats,records LEFT JOIN score_table ON recordnumber = record_id WHERE format = formatnumber "
+				+ cd_extra + " AND baseformat = ? AND simon_rank_count = ? AND boughtdate < 'today'::date - " + months
+				+ "*'1 month'::interval AND simon_score > 5  ORDER BY boughtdate DESC LIMIT 1";
+		PreparedStatement ps = Connect.getConnection().getPreparedStatement(sql);
+		ps.setString(1, baseformat);
+		ps.setInt(2, listenCount);
+		ResultSet rs = ps.executeQuery();
+		if (rs.next())
+			return GetRecords.create().getRecord(rs.getInt(1));
+
+		return null;
 	}
 
 }
