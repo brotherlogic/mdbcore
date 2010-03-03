96b8a1b42127395c221fbda60ddd55712abd86ef
Simon Tucker <s.tucker@dcs.shef.ac.uk>
2010-03-03 20:15:22 +0000

    Fixed bug in track reading in the discogs parser

--- src/main/java/uk/co/brotherlogic/mdb/parsers/DiscogParser.java
+++ src/main/java/uk/co/brotherlogic/mdb/parsers/DiscogParser.java
@@ -31,39 +31,32 @@ import uk.co.brotherlogic.mdb.label.Label;
 import uk.co.brotherlogic.mdb.record.Record;
 import uk.co.brotherlogic.mdb.record.Track;
 
-public class DiscogParser
-{
+public class DiscogParser {
 
-	public static void main(String[] args) throws Exception
-	{
+	public static void main(String[] args) throws Exception {
 		DiscogParser parser = new DiscogParser();
 		System.out.println(parser.parseDiscogRelease(808723));
 	}
 
 	String base = "http://www.discogs.com/release/ID?f=xml&api_key=67668099b8";
 
-	public Record parseDiscogRelease(int id) throws IOException
-	{
-		try
-		{
+	public Record parseDiscogRelease(int id) throws IOException {
+		try {
 			URL url = new URL(base.replace("ID", "" + id));
 			System.err.println("Reading: " + url);
 			SAXParser parser = SAXParserFactory.newInstance().newSAXParser();
 			DiscogXMLParser handler = new DiscogXMLParser();
 			parser.parse(new GZIPInputStream(url.openStream()), handler);
 			return handler.getRecord();
-		} catch (SAXException e)
-		{
+		} catch (SAXException e) {
 			throw new IOException(e);
-		} catch (ParserConfigurationException e)
-		{
+		} catch (ParserConfigurationException e) {
 			throw new IOException(e);
 		}
 	}
 }
 
-class DiscogXMLParser extends DefaultHandler
-{
+class DiscogXMLParser extends DefaultHandler {
 	Record rec = new Record();
 	List<LineUp> overallGroops = new LinkedList<LineUp>();
 	Track currTrack;
@@ -86,47 +79,39 @@ class DiscogXMLParser extends DefaultHandler
 
 	@Override
 	public void characters(char[] ch, int start, int length)
-			throws SAXException
-	{
+			throws SAXException {
 		text += new String(ch, start, length);
 	}
 
 	@Override
 	public void endElement(String uri, String localName, String qName)
-			throws SAXException
-	{
+			throws SAXException {
 		String qualName = localName + qName;
 
-		try
-		{
-			if (inMain)
-			{
+		try {
+			if (inMain) {
 				if (qualName.equals("artists"))
 					inArtists = false;
 				else if (qualName.equals("labels"))
 					inLabels = false;
 				else if (qualName.equals("formats"))
 					inFormats = false;
-				else if (inFormats && qualName.equals("description"))
-				{
+				else if (inFormats && qualName.equals("description")) {
 					if (text.equals("LP"))
-						switch (quantity)
-						{
+						switch (quantity) {
 						case 1:
 							rec
 									.setFormat(GetFormats.create().getFormat(
 											"12\""));
 							break;
 						}
-				} else if (qualName.equals("name") && inArtists)
-				{
+				} else if (qualName.equals("name") && inArtists) {
 					Groop grp = GetGroops.build().getGroopFromShowName(text);
 					if (grp == null)
 						grp = new Groop(text);
 					LineUp lup = null;
 					if (grp.getLineUps() == null
-							|| grp.getLineUps().size() == 0)
-					{
+							|| grp.getLineUps().size() == 0) {
 						Artist art = GetArtists.create().getArtistFromShowName(
 								text);
 						lup = new LineUp(grp);
@@ -137,21 +122,17 @@ class DiscogXMLParser extends DefaultHandler
 					overallGroops.add(lup);
 				} else if (qualName.equals("title"))
 					rec.setTitle(text);
-				else if (qualName.equals("description"))
-				{
+				else if (qualName.equals("description")) {
 					if (text.equals("Album"))
 						rec.setReleaseType(1);
-				} else if (qualName.equals("released"))
-				{
-					if (text.contains("-"))
-					{
+				} else if (qualName.equals("released")) {
+					if (text.contains("-")) {
 						String[] elems = text.split("-");
 						rec.setYear(Integer.parseInt(elems[0]));
 						rec.setReleaseMonth(Integer.parseInt(elems[1]));
 					} else
 						rec.setYear(Integer.parseInt(text));
-				} else if (qualName.equals("genre"))
-				{
+				} else if (qualName.equals("genre")) {
 					Category cat = GetCategories.build().getCategory(text);
 					if (text.equals("Rock"))
 						cat = GetCategories.build().getCategory("Rock & Pop");
@@ -159,17 +140,16 @@ class DiscogXMLParser extends DefaultHandler
 						rec.setCategory(cat);
 				}
 			} else if (inTracks)
-				if (qualName.equals("position"))
-				{
-					if (Character.isLetter(text.charAt(0)))
-					{
+				if (qualName.equals("position")) {
+					if (Character.isLetter(text.charAt(0))) {
+						if (text.trim().length() == 1)
+							text += "1";
 						int offsetCharacter = text.charAt(0) - ('A') + 1;
 						text = (offsetCharacter) + "-" + text.substring(1);
 					}
 
 					if (text.length() > 0)
-						if (text.contains("-"))
-						{
+						if (text.contains("-")) {
 							String[] elems = text.split("-");
 							int discNumber = Integer.parseInt(elems[0]);
 							int trckNumber = Integer.parseInt(elems[1]);
@@ -182,35 +162,30 @@ class DiscogXMLParser extends DefaultHandler
 										number));
 							else
 								highest.put(discNumber, number);
-						} else
-						{
+						} else {
 							int number = Integer.parseInt(text);
 							currTrack.setTrackNumber(number);
 						}
-				} else if (qualName.equals("track"))
-				{
+				} else if (qualName.equals("track")) {
 					rec.addTrack(currTrack);
 					if (currTrack.getLineUps().size() == 0)
 						currTrack.addLineUps(overallGroops);
 				} else if (qualName.equals("title"))
 					currTrack.setTitle(text);
 				else if (qualName.equals("duration"))
-					if (text.trim().length() > 0)
-					{
+					if (text.trim().length() > 0) {
 						String[] elems = text.split(":");
 						int lengthInSeconds = Integer.parseInt(elems[0]) * 60
 								+ Integer.parseInt(elems[1]);
 						currTrack.setLengthInSeconds(lengthInSeconds);
 					}
 
-		} catch (SQLException e)
-		{
+		} catch (SQLException e) {
 			throw new SAXException(e);
 		}
 	}
 
-	public Record getRecord()
-	{
+	public Record getRecord() {
 		// Set the author
 		rec.setAuthor(rec.getGroopString());
 
@@ -219,31 +194,26 @@ class DiscogXMLParser extends DefaultHandler
 
 	@Override
 	public void startElement(String uri, String localName, String qName,
-			Attributes attributes) throws SAXException
-	{
+			Attributes attributes) throws SAXException {
 		String qualName = localName + qName;
 		text = "";
 
-		try
-		{
+		try {
 			if (inMain)
 				if (qualName.equals("artists"))
 					inArtists = true;
 				else if (qualName.equals("labels"))
 					inLabels = true;
-				else if (qualName.equals("label") && inLabels)
-				{
+				else if (qualName.equals("label") && inLabels) {
 					String labelName = attributes.getValue("name");
 					String catNo = attributes.getValue("catno");
 					Label lab = GetLabels.create().getLabel(labelName);
 					rec.addLabel(lab);
 					rec.addCatNo(catNo);
-				} else if (qualName.equals("tracklist"))
-				{
+				} else if (qualName.equals("tracklist")) {
 					inMain = false;
 					inTracks = true;
-				} else if (qualName.equals("format"))
-				{
+				} else if (qualName.equals("format")) {
 					Format form = GetFormats.create().getFormat(
 							attributes.getValue("name"));
 					if (form != null)
@@ -257,13 +227,11 @@ class DiscogXMLParser extends DefaultHandler
 				if (qualName.equals("track"))
 					currTrack = new Track();
 
-			if (qualName.equals("release"))
-			{
+			if (qualName.equals("release")) {
 				rec.setDiscogsNum(Integer.parseInt(attributes.getValue("id")));
 				inMain = true;
 			}
-		} catch (SQLException e)
-		{
+		} catch (SQLException e) {
 			throw new SAXException(e);
 		}
 
