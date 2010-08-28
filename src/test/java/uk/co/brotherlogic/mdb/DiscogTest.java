d2c8aa7cce479b9ae31ddc18dffad80ef1722220
Simon Tucker <s.tucker@dcs.shef.ac.uk>
2010-08-28 07:46:43 +0100

    Added start of discog tests

---
+++ src/test/java/uk/co/brotherlogic/mdb/DiscogTest.java
@@ -0,0 +1,25 @@
+package uk.co.brotherlogic.mdb;
+
+import junit.framework.TestCase;
+import uk.co.brotherlogic.mdb.parsers.DiscogParser;
+import uk.co.brotherlogic.mdb.record.Record;
+
+public class DiscogTest extends TestCase {
+
+	public void testDiscog() {
+		DiscogParser parser = new DiscogParser();
+
+		// This is MV & EE - Drone Trailer
+		try {
+			Record r = parser.parseDiscogRelease(1642454);
+
+			// Check that the format tracks are correct
+			assert (r.getTracks().iterator().next().getFormTrackNumber() != -1);
+
+		} catch (Exception e) {
+			e.printStackTrace();
+			assert (false);
+		}
+	}
+
+}
