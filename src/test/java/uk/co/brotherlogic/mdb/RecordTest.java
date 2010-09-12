c0634a51957ee48b9abb56040de7d3969d56ede9
Simon Tucker <s.tucker@dcs.shef.ac.uk>
2010-09-12 14:32:00 +0100

    Fixed a discogs issue

--- src/test/java/uk/co/brotherlogic/mdb/RecordTest.java
+++ src/test/java/uk/co/brotherlogic/mdb/RecordTest.java
@@ -13,16 +13,20 @@ import uk.co.brotherlogic.mdb.label.Label;
 import uk.co.brotherlogic.mdb.record.GetRecords;
 import uk.co.brotherlogic.mdb.record.Record;
 
-public class RecordTest extends TestCase {
+public class RecordTest extends TestCase
+{
 	private static boolean built = false;
 
-	public RecordTest() {
+	public RecordTest()
+	{
 		super();
 		Connect.setForDevMode();
 	}
 
-	private void buildRecord() {
-		if (!built) {
+	private void buildRecord()
+	{
+		if (!built)
+		{
 			// Create
 			Record r = new Record();
 			r.setAuthor("fake-author");
@@ -41,57 +45,80 @@ public class RecordTest extends TestCase {
 			r.setYear(2000);
 			r.setDiscogsNum(1234);
 
-			try {
+			try
+			{
 				// Persist
 				r.save();
 				built = true;
 
 				// Set some scores
 				r.addScore(User.getUser("Simon"), 5);
-			} catch (SQLException e) {
+			}
+			catch (SQLException e)
+			{
 				e.printStackTrace();
 			}
 		}
 	}
 
-	public void testCatNoSize() {
-		try {
+	public void testCatNoSize()
+	{
+		try
+		{
 			buildRecord();
 			Record nrec = GetRecords.create().getRecords("fake-title").get(0);
 			assert (nrec.getCatNos().size() == 1);
 			assert (nrec.getCatNoString().equals("fake-cat-no"));
-		} catch (SQLException e) {
+		}
+		catch (SQLException e)
+		{
 			e.printStackTrace();
 			assert (false);
 		}
 	}
 
-	public void testDiscog() {
-		try {
+	public void testDiscog()
+	{
+		try
+		{
 			buildRecord();
 			Record nrec = GetRecords.create().getRecords("fake-title").get(0);
+			assert (nrec.getCatNos().size() == 1);
+			assert (nrec.getLabels().size() == 1);
 			assert (nrec.getDiscogsNum() == 1234);
-		} catch (SQLException e) {
+			nrec.setDiscogsNum(1235);
+			nrec.save();
+			Record nrec2 = GetRecords.create().getRecords("fake-title").get(0);
+			assert (nrec2.getDiscogsNum() == 1235);
+		}
+		catch (SQLException e)
+		{
 			e.printStackTrace();
 			assert (false);
 		}
 	}
 
-	public void testLabels() {
-		try {
+	public void testLabels()
+	{
+		try
+		{
 			buildRecord();
 			Record nrec = GetRecords.create().getRecords("fake-title").get(0);
 			assert (nrec.getLabels().size() == 1);
 			assert (nrec.getLabels().iterator().next().getName()
 					.equals("fake-label"));
-		} catch (SQLException e) {
+		}
+		catch (SQLException e)
+		{
 			e.printStackTrace();
 			assert (false);
 		}
 	}
 
-	public void testNewAuthor() {
-		try {
+	public void testNewAuthor()
+	{
+		try
+		{
 			buildRecord();
 			Record r = GetRecords.create().getRecords("fake-title").get(0);
 			r.setAuthor("New Fake Author");
@@ -99,14 +126,18 @@ public class RecordTest extends TestCase {
 			Record nrec = GetRecords.create().getRecords("fake-title").get(0);
 			assert (nrec.getAuthor().equals("New Fake Author"));
 
-		} catch (SQLException e) {
+		}
+		catch (SQLException e)
+		{
 			e.printStackTrace();
 			assert (false);
 		}
 	}
 
-	public void testOverlap() {
-		try {
+	public void testOverlap()
+	{
+		try
+		{
 			Collection<Record> records = GetRecords.create().getRecords(
 					"Overload");
 			List<String> overloadReps = new LinkedList<String>();
@@ -114,32 +145,41 @@ public class RecordTest extends TestCase {
 				if (rec.getFormat().getBaseFormat().equals("CD"))
 					overloadReps.add(rec.getFileAdd());
 			for (int i = 0; i < overloadReps.size(); i++)
-				for (int j = i + 1; j < overloadReps.size(); j++) {
+				for (int j = i + 1; j < overloadReps.size(); j++)
+				{
 					if (overloadReps.get(i).equals(overloadReps.get(j)))
 						System.err.println(overloadReps.get(i) + " vs "
 								+ overloadReps.get(j));
 					assert (!overloadReps.get(i).equals(overloadReps.get(j)));
 				}
-		} catch (SQLException e) {
+		}
+		catch (SQLException e)
+		{
 			e.printStackTrace();
 			assert (false);
 		}
 	}
 
-	public void testRetrieveSize() {
-		try {
+	public void testRetrieveSize()
+	{
+		try
+		{
 			buildRecord();
 			// Retrieve
 			List<Record> recs = GetRecords.create().getRecords("fake-title");
 			assert (recs.size() == 1);
-		} catch (SQLException e) {
+		}
+		catch (SQLException e)
+		{
 			e.printStackTrace();
 			assert (false);
 		}
 	}
 
-	public void testRiploc() {
-		try {
+	public void testRiploc()
+	{
+		try
+		{
 			buildRecord();
 			// Retrieve
 			Record rec = GetRecords.create().getRecords("fake-title").get(0);
@@ -148,32 +188,42 @@ public class RecordTest extends TestCase {
 
 			Record rec2 = GetRecords.create().getRecords("fake-title").get(0);
 			assert (rec2.getRiploc().equals("testing"));
-		} catch (SQLException e) {
+		}
+		catch (SQLException e)
+		{
 			e.printStackTrace();
 			assert (false);
 		}
 	}
 
-	public void testScore() {
-		try {
+	public void testScore()
+	{
+		try
+		{
 			buildRecord();
 			Record nrec = GetRecords.create().getRecords("fake-title").get(0);
 			double score = nrec.getScore();
 			double sscore = nrec.getScore(User.getUser("Simon"));
 			assert (score >= 0);
 			assert (sscore >= 0);
-		} catch (SQLException e) {
+		}
+		catch (SQLException e)
+		{
 			e.printStackTrace();
 			assert (false);
 		}
 	}
 
-	public void testWeaver() {
-		try {
-			Record weaver = GetRecords.create()
-					.getRecords("The Fallen By Watch Bird").get(0);
+	public void testWeaver()
+	{
+		try
+		{
+			Record weaver = GetRecords.create().getRecords(
+					"The Fallen By Watch Bird").get(0);
 			assert (!weaver.getTrackRep(1).contains(Record.REPLACE));
-		} catch (SQLException e) {
+		}
+		catch (SQLException e)
+		{
 			e.printStackTrace();
 			assert (false);
 		}
