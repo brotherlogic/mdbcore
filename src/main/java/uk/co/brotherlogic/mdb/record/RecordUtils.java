15bcd4a5061721c69bef222cb9f80b3376e669d3
Simon Tucker <s.tucker@dcs.shef.ac.uk>
2010-09-19 20:08:14 +0100

    Fixed get records bug

--- src/main/java/uk/co/brotherlogic/mdb/record/RecordUtils.java
+++ src/main/java/uk/co/brotherlogic/mdb/record/RecordUtils.java
@@ -9,10 +9,8 @@ import java.util.List;
 
 import uk.co.brotherlogic.mdb.Connect;
 
-public class RecordUtils
-{
-	public static List<Record> getIpodRecords() throws SQLException
-	{
+public class RecordUtils {
+	public static List<Record> getIpodRecords() throws SQLException {
 		String sql = " select recordnumber,riploc,recrand,COUNT(score_value) as cnt,AVG(score_value) as val from records left join score_history ON records.recordnumber = record_id WHERE riploc IS NOT NULL AND user_id = 1 GROUP BY recordnumber,recrand,riploc HAVING count(score_value) = 1 ORDER by val DESC,recrand DESC LIMIT 5";
 		List<Record> rec = new LinkedList<Record>();
 
@@ -25,8 +23,7 @@ public class RecordUtils
 		return rec;
 	}
 
-	private static Record getNewRecord(String baseformat) throws SQLException
-	{
+	private static Record getNewRecord(String baseformat) throws SQLException {
 		String cd_extra = "AND riploc IS NOT NULL";
 
 		if (!baseformat.equalsIgnoreCase("cd"))
@@ -44,8 +41,7 @@ public class RecordUtils
 	}
 
 	public static List<Record> getNewRecords(String baseformat)
-			throws SQLException
-	{
+			throws SQLException {
 		String cd_extra = "AND riploc IS NOT NULL";
 
 		if (!baseformat.equalsIgnoreCase("cd"))
@@ -63,16 +59,14 @@ public class RecordUtils
 		return records;
 	}
 
-	public static Track getRandomCDTrack(double minScore) throws SQLException
-	{
+	public static Track getRandomCDTrack(double minScore) throws SQLException {
 		// Get a random record
 		String sql = "SELECT recordnumber from records LEFT JOIN score_history ON recordnumber = record_id WHERE riploc IS NOT NULL GROUP BY recordnumber HAVING avg(score_value) > ? ORDER BY random() LIMIT 1";
 		PreparedStatement ps = Connect.getConnection()
 				.getPreparedStatement(sql);
 		ps.setDouble(1, minScore);
 		ResultSet rs = Connect.getConnection().executeQuery(ps);
-		if (rs.next())
-		{
+		if (rs.next()) {
 			Record r = GetRecords.create().getRecord(rs.getInt(1));
 			List<Track> tracks = new LinkedList<Track>(r.getTracks());
 			Collections.shuffle(tracks);
@@ -82,8 +76,7 @@ public class RecordUtils
 		return null;
 	}
 
-	public static Record getRecord(String baseformat) throws SQLException
-	{
+	public static Record getRecord(String baseformat) throws SQLException {
 
 		String cd_extra = "AND riploc IS NOT NULL";
 		if (!baseformat.equalsIgnoreCase("cd"))
@@ -91,7 +84,7 @@ public class RecordUtils
 
 		String sql = "select recordnumber,count(score_value) as cnt,avg(score_value) AS mean  from records,score_history,formats WHERE format = formatnumber AND "
 				+ cd_extra
-				+ "baseformat = ? AND recordnumber = record_id AND user_id =1 GROUP BY recrand,recordnumber HAVING count(score_value) = 1 ORDER BY avg(score_value) DESC, recrand ASC LIMIT 10";
+				+ "baseformat = ? AND boughtdate < (now() - interval '3 months') AND recordnumber = record_id AND user_id =1 GROUP BY recrand,recordnumber HAVING count(score_value) = 1 ORDER BY avg(score_value) DESC, recrand ASC LIMIT 10";
 		PreparedStatement ps = Connect.getConnection()
 				.getPreparedStatement(sql);
 		ps.setString(1, baseformat);
@@ -103,8 +96,7 @@ public class RecordUtils
 	}
 
 	public static List<Record> getRecords(String baseformat, int num)
-			throws SQLException
-	{
+			throws SQLException {
 
 		String cd_extra = "AND riploc IS NOT NULL";
 		if (!baseformat.equalsIgnoreCase("cd"))
@@ -112,7 +104,7 @@ public class RecordUtils
 
 		String sql = "select recordnumber,count(score_value) as cnt,avg(score_value) AS mean from records,score_history,formats WHERE format = formatnumber "
 				+ cd_extra
-				+ " AND baseformat = ? AND recordnumber = record_id AND salepricepence < 0 AND user_id =1 GROUP BY recrand,recordnumber HAVING count(score_value) = 1 "
+				+ " AND baseformat = ? AND boughtdate < (now() - interval '3 months') AND recordnumber = record_id AND salepricepence < 0 AND user_id =1 GROUP BY recrand,recordnumber HAVING count(score_value) = 1 "
 				+ "ORDER BY avg(score_value) DESC, recrand ASC LIMIT " + num;
 		PreparedStatement ps = Connect.getConnection()
 				.getPreparedStatement(sql);
@@ -126,8 +118,7 @@ public class RecordUtils
 	}
 
 	public static Record getRecordToListenTo(String baseformat)
-			throws SQLException
-	{
+			throws SQLException {
 		// Always favour new records
 		Record r = getNewRecord(baseformat);
 		// r = getRecord(baseformat, 2, 6);
@@ -137,13 +128,11 @@ public class RecordUtils
 	}
 
 	public static Record getRecordToListenTo(String[] baseformats)
-			throws SQLException
-	{
+			throws SQLException {
 
 		Record newRecord = null;
 		Record currRecord = null;
-		for (String string : baseformats)
-		{
+		for (String string : baseformats) {
 			Record tempNew = getNewRecord(string);
 			Record currRec = getRecordToListenTo(string);
 			{
@@ -164,14 +153,12 @@ public class RecordUtils
 		return currRecord;
 	}
 
-	public static Record getRecordToRip() throws SQLException
-	{
+	public static Record getRecordToRip() throws SQLException {
 		String sql = "SELECT recordnumber from records,formats WHERE baseformat = 'CD' AND format = formatnumber AND riploc IS NULL ORDER BY owner";
 		PreparedStatement ps = Connect.getConnection()
 				.getPreparedStatement(sql);
 		ResultSet rs = ps.executeQuery();
-		while (rs.next())
-		{
+		while (rs.next()) {
 			Record rec = (GetRecords.create().getRecord(rs.getInt(1)));
 			if (!rec.getFormat().getName().equals("DVD"))
 				return rec;
@@ -179,8 +166,7 @@ public class RecordUtils
 		return null;
 	}
 
-	public static void main(String[] args) throws Exception
-	{
+	public static void main(String[] args) throws Exception {
 		for (Record rec : RecordUtils.getRecords("12", 10))
 			System.out.println(rec.getAuthor() + " - " + rec.getTitle());
 	}
