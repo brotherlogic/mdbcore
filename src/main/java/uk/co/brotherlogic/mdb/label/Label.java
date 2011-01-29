74b905b701f667da2d970f4465436670a6d0a1c1
Simon Tucker <s.tucker@dcs.shef.ac.uk>
2011-01-29 06:01:02 +0000

    Added get number for labels

--- src/main/java/uk/co/brotherlogic/mdb/label/Label.java
+++ src/main/java/uk/co/brotherlogic/mdb/label/Label.java
@@ -11,95 +11,110 @@ import uk.co.brotherlogic.mdb.record.Record;
  * 
  * @author Simon Tucker
  */
-public class Label implements Comparable<Label>, Serializable {
-	/**
+public class Label implements Comparable<Label>, Serializable
+{
+   /**
 	 * 
 	 */
-	private static final long serialVersionUID = -7750109574923648363L;
+   private static final long serialVersionUID = -7750109574923648363L;
 
-	/**
-	 * The label name
-	 */
-	private final String labelName;
+   /**
+    * The label name
+    */
+   private final String labelName;
 
-	/** The label number */
-	private int labNo;
+   /** The label number */
+   private int labNo;
 
-	/**
-	 * Constructor
-	 * 
-	 * @param name
-	 *            The name of the label
-	 */
-	public Label(final String name) {
-		this(name, -1);
-	}
+   /**
+    * Constructor
+    * 
+    * @param name
+    *           The name of the label
+    */
+   public Label(final String name)
+   {
+      this(name, -1);
+   }
 
-	/**
-	 * Constructor
-	 * 
-	 * @param name
-	 *            The name of the label
-	 * @param number
-	 *            The number of the label
-	 */
-	public Label(final String name, final int number) {
-		labelName = name;
-		labNo = number;
-	}
+   /**
+    * Constructor
+    * 
+    * @param name
+    *           The name of the label
+    * @param number
+    *           The number of the label
+    */
+   public Label(final String name, final int number)
+   {
+      labelName = name;
+      labNo = number;
+   }
 
-	@Override
-	public final int compareTo(final Label o) {
-		return -labelName.toLowerCase().compareTo(o.labelName.toLowerCase());
-	}
+   @Override
+   public final int compareTo(final Label o)
+   {
+      return -labelName.toLowerCase().compareTo(o.labelName.toLowerCase());
+   }
 
-	@Override
-	public final boolean equals(final Object o) {
-		if (o instanceof Label)
-			return this.compareTo((Label) o) == 0;
-		else
-			return false;
-	}
+   @Override
+   public final boolean equals(final Object o)
+   {
+      if (o instanceof Label)
+         return this.compareTo((Label) o) == 0;
+      else
+         return false;
+   }
 
-	/**
-	 * Gets the name of the label
-	 * 
-	 * @return The name of the label
-	 */
-	public final String getName() {
-		return labelName;
-	}
+   /**
+    * Gets the name of the label
+    * 
+    * @return The name of the label
+    */
+   public final String getName()
+   {
+      return labelName;
+   }
 
-	/**
-	 * Gets the associated records of this label
-	 * 
-	 * @return The collection of records that have this label
-	 * @throws SQLException
-	 *             If the DB connection fails
-	 */
-	public final Collection<Record> getRecords() throws SQLException {
-		return GetLabels.create().getRecords(labNo);
-	}
+   public final int getNumber()
+   {
+      return labNo;
+   }
 
-	@Override
-	public final int hashCode() {
-		return labelName.hashCode();
-	}
+   /**
+    * Gets the associated records of this label
+    * 
+    * @return The collection of records that have this label
+    * @throws SQLException
+    *            If the DB connection fails
+    */
+   public final Collection<Record> getRecords() throws SQLException
+   {
+      return GetLabels.create().getRecords(labNo);
+   }
 
-	/**
-	 * Saves the label to the DB
-	 * 
-	 * @throws SQLException
-	 *             if the DB connection fails
-	 */
-	public final int save() throws SQLException {
-		if (labNo == -1)
-			labNo = GetLabels.create().addLabel(this);
-		return labNo;
-	}
+   @Override
+   public final int hashCode()
+   {
+      return labelName.hashCode();
+   }
 
-	@Override
-	public final String toString() {
-		return labelName;
-	}
+   /**
+    * Saves the label to the DB
+    * 
+    * @throws SQLException
+    *            if the DB connection fails
+    */
+   public final int save() throws SQLException
+   {
+      if (labNo == -1)
+         labNo = GetLabels.create().addLabel(this);
+      return labNo;
+   }
+
+   @Override
+   public final String toString()
+   {
+      return labelName;
+   }
 }
