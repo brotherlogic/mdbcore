a2a550a20cac9f768239fbe728b5f164eb96476a
Simon Tucker <s.tucker@dcs.shef.ac.uk>
2012-01-28 21:08:30 +0000

    Removed J from the users table

--- src/main/java/uk/co/brotherlogic/mdb/User.java
+++ src/main/java/uk/co/brotherlogic/mdb/User.java
@@ -7,70 +7,84 @@ import java.util.Collection;
 import java.util.LinkedList;
 import java.util.List;
 
-public class User {
+public class User
+{
 
-	public static User getUser(int id) {
-		try {
-			String sql = "SELECT name from user_table where user_id = ?";
-			PreparedStatement ps = Connect.getConnection()
-					.getPreparedStatement(sql);
-			ps.setInt(1, id);
-			ResultSet rs = ps.executeQuery();
-			while (rs.next())
-				return new User(id, rs.getString(1));
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
+   public static User getUser(int id)
+   {
+      try
+      {
+         String sql = "SELECT name from user_table where user_id = ?";
+         PreparedStatement ps = Connect.getConnection().getPreparedStatement(sql);
+         ps.setInt(1, id);
+         ResultSet rs = ps.executeQuery();
+         while (rs.next())
+            return new User(id, rs.getString(1));
+      }
+      catch (SQLException e)
+      {
+         e.printStackTrace();
+      }
 
-		return null;
-	}
+      return null;
+   }
 
-	public static User getUser(String id) {
-		try {
-			String sql = "SELECT user_id from user_table where lower(name) = ?";
-			PreparedStatement ps = Connect.getConnection()
-					.getPreparedStatement(sql);
-			ps.setString(1, id.toLowerCase());
-			ResultSet rs = ps.executeQuery();
-			while (rs.next())
-				return new User(rs.getInt(1), id);
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
+   public static User getUser(String id)
+   {
+      try
+      {
+         String sql = "SELECT user_id from user_table where lower(name) = ?";
+         PreparedStatement ps = Connect.getConnection().getPreparedStatement(sql);
+         ps.setString(1, id.toLowerCase());
+         ResultSet rs = ps.executeQuery();
+         while (rs.next())
+            return new User(rs.getInt(1), id);
+      }
+      catch (SQLException e)
+      {
+         e.printStackTrace();
+      }
 
-		return null;
-	}
-	
-	public static Collection<User> getUsers(){
-		List<User> users = new LinkedList<User>();
-		try {
-			String sql = "SELECT user_id,name from user_table";
-			PreparedStatement ps = Connect.getConnection()
-					.getPreparedStatement(sql);
-			ResultSet rs = ps.executeQuery();
-			while (rs.next())
-				users.add(new User(rs.getInt(1), rs.getString(2)));
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
+      return null;
+   }
 
-		return users;
-	}
+   public static Collection<User> getUsers()
+   {
+      List<User> users = new LinkedList<User>();
+      try
+      {
+         String sql = "SELECT user_id,name from user_table";
+         PreparedStatement ps = Connect.getConnection().getPreparedStatement(sql);
+         ResultSet rs = ps.executeQuery();
+         while (rs.next())
+            if (rs.getString(2).contains("imon"))
+               users.add(new User(rs.getInt(1), rs.getString(2)));
+      }
+      catch (SQLException e)
+      {
+         e.printStackTrace();
+      }
 
-	private final int id;
+      return users;
+   }
 
-	public String getName() {
-		return name;
-	}
+   private final int id;
 
-	private final String name;
+   private final String name;
 
-	public User(int id, String name) {
-		this.id = id;
-		this.name = name;
-	}
+   public User(int id, String name)
+   {
+      this.id = id;
+      this.name = name;
+   }
 
-	public int getID() {
-		return id;
-	}
+   public int getID()
+   {
+      return id;
+   }
+
+   public String getName()
+   {
+      return name;
+   }
 }
